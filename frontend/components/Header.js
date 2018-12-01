3f6f0a1b0b7a9e133b775e12aef472eb6414e867
Albert Agram <albert.agram@gmail.com>
2018-12-01 16:50:53 -0500

    custom Layout

---
+++ frontend/components/Header.js
@@ -0,0 +1,16 @@
+import Nav from "./Nav";
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Kids Shoe Port</a>
+    </div>
+    <Nav />
+    <div className="sub-bar">
+      <p>Cart</p>
+    </div>
+    <div>Search</div>
+  </div>
+);
+
+export default Header;
